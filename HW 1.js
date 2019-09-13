5cc9778755acd7949668d564d172b930108ce530
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-09-13 11:12:08 -0400

    HW 1

---
+++ HW 1.js
@@ -0,0 +1,45 @@
+/*let x = 2 + '2';
+
+console.log(x);
+*/ /// Console: 22
+
+/*let x = 2 + 2 + '2';
+
+console.log(x);
+*/ /// Console: 42
+
+/*let x = 2 + true;
+
+console.log(x);
+*/ /// Console: 3
+
+/*let x = 2 + true + '2';
+
+console.log(x);
+*/ /// Console: 32
+
+/*let x = true + '2';
+
+console.log(x);
+*/ /// Console: true2
+
+/*let x = Boolean(1,2,3,4...);
+
+console.log(x);
+*/ /// Console: true
+
+/*let x = Boolean(0);
+
+console.log(x);
+*/ /// Console: false
+
+
+let x = '';
+
+if((x !== undefined) || (x !== null) || (x !== 0)){
+
+} 
+
+if(x){
+    
+}
